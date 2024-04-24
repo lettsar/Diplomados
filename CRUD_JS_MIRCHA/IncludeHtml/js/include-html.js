e10910aae633fce61e65f74e2a3bacf047343f13
Alfredo Leonett <lettsar@gmail.com>
2024-04-23 23:09:40 -0400

    Includehtml

---
+++ CRUD_JS_MIRCHA/IncludeHtml/js/include-html.js
@@ -0,0 +1,37 @@
+const attr = document.querySelectorAll("[data-include]")
+attr.forEach(element => {
+console.log(element.getAttribute("data-include"))  
+});
+const IncludeHtml = (el,url)=>{
+
+  xhr.addEventListener("readystatechange", e=>{
+    if(xhr.readyState !== 4) return;
+
+    if(xhr.status >=200 && xhr.status <= 300){
+        el.outerHTML = xhr.responseText;
+    }else{
+        let message = xhr.statusText || "Ocurrio un Error";
+        el.outerHTML =`<div><p>Error: ${xhr.status}: ${message}</p></div>`;
+    }
+});
+xhr.open("GET",url);
+xhr.setRequestHeader("Content-Type", "text-html; charset=utf-8")
+xhr.send();
+}
+
+document.addEventListener("DOMContentLoaded",e=>{
+
+})
+
+console.log()
+
+/* document.addEventListener("DOMContentLoaded", e=>{
+
+    const includeHTML = (el,url)=>{
+
+}
+
+ attr.forEach(element => {
+   console.log(element.getAttribute("[data-include]"))
+ });
+}) */
