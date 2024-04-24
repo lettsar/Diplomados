0bbfb9f9b248ace68ea8c5c1c8d3ba1616aec388
Alfredo Leonett <lettsar@gmail.com>
2024-04-23 23:57:46 -0400

    Modificado git commit -m Modificado git push -u origin master git commit -m Modificado

--- CRUD_JS_MIRCHA/IncludeHtml/js/include-html.js
+++ CRUD_JS_MIRCHA/IncludeHtml/js/include-html.js
@@ -1,14 +1,11 @@
-const attr = document.querySelectorAll("[data-include]")
-attr.forEach(element => {
-console.log(element.getAttribute("data-include"))  
-});
 const IncludeHtml = (el,url)=>{
-
+  const xhr = new XMLHttpRequest()
   xhr.addEventListener("readystatechange", e=>{
     if(xhr.readyState !== 4) return;
 
     if(xhr.status >=200 && xhr.status <= 300){
         el.outerHTML = xhr.responseText;
+        console.log()
     }else{
         let message = xhr.statusText || "Ocurrio un Error";
         el.outerHTML =`<div><p>Error: ${xhr.status}: ${message}</p></div>`;
@@ -19,8 +16,13 @@ xhr.setRequestHeader("Content-Type", "text-html; charset=utf-8")
 xhr.send();
 }
 
-document.addEventListener("DOMContentLoaded",e=>{
+document.addEventListener("DOMContentLoaded",e =>{
+const attr = document.querySelectorAll("[data-include]")
+attr.forEach(el => {
+console.log(el.getAttribute("data-include"))
+  IncludeHtml(el,el.getAttribute("data-include"));
 
+})
 })
 
 console.log()
