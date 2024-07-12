df0c2628b5a1567cd0981e4a5491e0983a9f5f69
Alfredo Leonett <lettsar@gmail.com>
2024-07-11 21:13:40 -0400

    Node

---
+++ nodeJhon/03_NPM/calculadora.js
@@ -0,0 +1,15 @@
+const Sumar=(a,b)=> a+b ;
+const Restar =(a,b)=> a-b ;
+const Multiplicar =(a,b)=> a*b ;
+const Dividir =(a,b)=> a/b ;
+const modulo =(a,b)=> a%b ;
+
+export const caluladora ={
+    Sumar,
+    Restar,
+    Multiplicar,
+    Dividir,
+    modulo
+}
+
+ 
